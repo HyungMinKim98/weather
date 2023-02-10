f897da1606b03384c8e3c0b0f3e3fcbf79bf2e3c
HyungMinKim98 <you@example.com>
2023-02-10 17:43:21 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import 'react-open-weather-widget/lib/css/ReactWeather.css';
 import OpenWeather from 'react-open-weather-widget';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AreaSearch from './components/AreaSearch';
 function App() {
-  const [areaInfo,setAreaInfo]= useState({
-    lat:'37.566535',
-    lon:'126.9779692'
-  })
+  const [areaInfo,setAreaInfo]= useState(undefined)
+
+  const currentPostion=() =>{
+    const positionCurrent = navigator.geolocation.getCurrentPosition((position)=>{
+      const {latitude, longitude} = position.coords;
+      console.log(latitude,longitude)
+      setAreaInfo({lat: String(latitude), lon:String(longitude) })
+    })
+  }
+    useEffect(()=>{
+      currentPostion();
+    },[])
+    
   return (
     <div className="App">
       <AreaSearch setAreaInfo={setAreaInfo} />
